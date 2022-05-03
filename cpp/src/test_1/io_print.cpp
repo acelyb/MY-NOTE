24d92a4bd62f58f695b1366e73b469698a1ba97f
acelyb <lyb961201@foxmail.com>
2022-05-03 23:40:51 +0800

    cpp 学习

---
+++ cpp/src/test_1/io_print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int foo() {
+  cout << "this is foo." << endl;
+  return 0;
+}
+
+int main() {
+  cout << "Hello World!" << endl;
+  foo();
+  return 0;
+}
